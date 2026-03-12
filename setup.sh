3f3b9180724341cc7b15a3b1fc2065b38f44c3f9
PranavDeepakSathya <sathya.pranav.deepak@gmail.com>
2026-03-12 11:16:15 +0000

    okay_we_need_to_chill

--- setup.sh
+++ setup.sh
@@ -5,11 +5,14 @@ source .venv/bin/activate
 pip install --upgrade pip setuptools wheel
 pip install nvcc4jupyter numpy jupyter_client ipykernel ipywidgets
 pip install triton
-pip install torch
+pip3 install torch torchvision --index-url https://download.pytorch.org/whl/cu130
 pip install matplotlib
 pip install pandas
 pip install nvidia-cutlass-dsl
-pip install pufferlib
+pip install pufferlib --no-build-isolation
+pip install stable-baselines3 
+pip install wandb
+
 
 
 git config --global user.email sathya.pranav.deepak@gmail.com
